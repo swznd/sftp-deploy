3725dee188f221ff1081e705efb490bbcb13c7d8
Muhammad Fikri <fikswznd@gmail.com>
2020-04-18 21:33:38 +0700

    fix filter

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const path = require('path');
     const renamed = await git('diff', '--name-only', '--diff-filter=R', start, end);
     const deleted = await git('diff', '--name-only', '--diff-filter=D', start, end);
   
-    const filterFile = file => file !== '' && (['', './', '.'].indexOf(localPath) === -1 && file.startsWith(localPath)) && (ignore && !micromatch.isMatch(file, ignore));
+    const filterFile = file => file !== '' && (['', './', '.'].indexOf(localPath) !== -1 && file.startsWith(localPath)) && (ignore && !micromatch.isMatch(file, ignore));
 
     const filteredModified = modified.split("\n").filter(filterFile);
     const fileteredRenamed = renamed.split("\n").filter(filterFile);
